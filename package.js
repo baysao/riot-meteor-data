68d2bb5474803536b1760d047044f47421bf6300
Vu Tran <baysao@gmail.com>
2015-11-19 11:10:30 +0700

    update riotjs version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "baysao:riot-meteor-data",
   summary: "Riotjs mixin for reactively tracking Meteor data",
-  version: '0.1.0-3',
+  version: '0.1.0-5',
   documentation: 'README.md',
   git: 'https://github.com/baysao/riot-meteor-data.git'
 });
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.1.0.2');
   api.use('tracker');
-  api.use('baysao:riotjs@2.2.4-1', ['client', 'server']);
+  api.use('baysao:riotjs@2.3.1-4', ['client', 'server']);
   api.use('jsx@0.1.6');
 
   api.export(['RiotMeteorData']);
